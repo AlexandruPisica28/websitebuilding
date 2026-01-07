6413665c326c1d5b688fb26453734e76b0e5c11f
Alexandru Pisica <alexandrupisica@192.168.1.8>
2026-01-07 15:08:47 +0200

    ws

---
+++ curs3/rest.js
@@ -0,0 +1,24 @@
+// Get all users
+function httpGet(){
+    var url  = "https://reqres.in/api/users?page=1";
+    var xhr  = new XMLHttpRequest();
+    xhr.open('GET', url, true)
+    //xhr.readystatechange = function () {
+    //https://teamtreehouse.com/community/xhronreadystatechange-vs-xhronload
+    xhr.onload = function () {
+        var users = JSON.parse(xhr.responseText);
+        let userCard = '';
+        const content = document.getElementById('continut');
+            if (xhr.readyState == 4 && xhr.status == "200"){
+            console.table(users);
+            let myUsers = users.data;
+            for (let i = 0; i <= myUsers.length; i++) {
+            
+            }
+        } else {
+            console.error(users);
+        }
+    }
+    xhr.send(null);
+    }
+    
